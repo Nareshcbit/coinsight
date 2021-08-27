1e3e44b645715930bbbc0fbf37eca161e874a590
Naresh Madiraju <naresh.johnny@gmail.com>
2021-08-27 10:22:45 -0400

    columnGroupshow open cl;osed always

--- dashboard/coindata.js
+++ dashboard/coindata.js
@@ -4,39 +4,31 @@ const coindataColumnDefs = [
   { field: "id" },
   { field: "symbol" },
   { field: "name" },
+  {headerName: '4Weeks_Adds.',field: "developer_data.code_additions_deletions_4_weeks.additions"},
+  {
+    headerName: 'Score',
+    groupId: 'scoreGroup',
+    children: [
+        // using medal column type
+        { headerName: 'coingecko', field: 'coingecko_score' , columnGroupShow: 'always'},
+        { headerName: 'developer', field: 'developer_score' , columnGroupShow: 'open'},
+        { headerName: 'community', field: 'community_score' , columnGroupShow: 'open'},
+        { headerName: 'liquidity', field: 'liquidity_score' , columnGroupShow: 'open'},
+    ],
+  },
 ];
-
-
-const columnDefs = [
-  { field: "make" },
-  { field: "model" },
-  { field: "price" }
-];
-
-// specify the data
-const rowData = [
-  { make: "Toyota", model: "Celica", price: 35000 },
-  { make: "Ford", model: "Mondeo", price: 32000 },
-  { make: "Porsche", model: "Boxter", price: 72000 }
-];
-
-// let the grid know which columns and what data to use
-const gridOptions = {
-  columnDefs: columnDefs,
-  rowData: rowData
-};
 
 function tableCoinData(coinids, days){
   COIN_DATA = []
   let deferreds = []
   coinids.forEach((coinid)=>{
-    console.log("coinid: " + coinid)
+    //console.log("coinid: " + coinid)
     deferreds.push(ajaxCoinData(coinid, days))
   })
   $.when.apply(null, deferreds).done(function() {
-    console.log("all coins done")
-    console.log(JSON.stringify(COIN_DATA))
+    //console.log(JSON.stringify(COIN_DATA))
 
+    console.log(JSON.stringify(COIN_DATA))
     let coindataGridOptions = {
       columnDefs: coindataColumnDefs,
       rowData: COIN_DATA
@@ -46,11 +38,9 @@ function tableCoinData(coinids, days){
     $('#coindataDiv').find('#coindataGrid').each(function() {
       $(this).remove()
     });
-    $('#coindataDiv').append('<div id="coindataGrid" style="height: 200px; width:500px;" class="ag-theme-alpine">');
+    $('#coindataDiv').append('<div id="coindataGrid" style="height: 200px;" class="ag-theme-alpine">');
     
-    const gridDiv = document.querySelector('#coindataGrid');
-    console.log("gridDiv: " + gridDiv)
-    console.log(JSON.stringify(gridOptions))
+    let gridDiv = document.querySelector('#coindataGrid');
     new agGrid.Grid(gridDiv, coindataGridOptions);
     
   })
@@ -66,7 +56,7 @@ function ajaxCoinData(coinid, days){
   return  $.ajax({
     url: url,
     success: function(result){
-     console.log(JSON.stringify(result))
+     //console.log(JSON.stringify(result))
      COIN_DATA.push(result)
     }
   });
